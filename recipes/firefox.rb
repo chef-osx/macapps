0e45b4637ea98aba9a854125976894bc8a333d80
Julian C. Dunn <jdunn@getchef.com>
2014-01-02 12:46:15 -0500

    Correct cargo-cult dmg name

--- recipes/firefox.rb
+++ recipes/firefox.rb
@@ -16,7 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-dmg_properties = node['ud-macapps']['dropbox']['dmg']
+dmg_properties = node['ud-macapps']['firefox']['dmg']
 
 dmg_package "Firefox" do
   source dmg_properties['source']
