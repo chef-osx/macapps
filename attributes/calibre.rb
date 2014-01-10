5e4e226278edd926e50c37928b317a853851c245
Julian C. Dunn <jdunn@aquezada.com>
2014-01-09 19:08:27 -0500

    Added Calibre e-book conversion software

---
+++ attributes/calibre.rb
@@ -0,0 +1,2 @@
+default['ud-macapps']['calibre']['dmg']['source'] = 'http://files.calibre-ebook.com/1.18.0/calibre-1.18.0.dmg'
+default['ud-macapps']['calibre']['dmg']['checksum'] = '61ca2b7c1ee77b146de64e72d82464ac0896bbbdaa13ca96fea5048b61d0a3ea'
