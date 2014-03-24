6a22ab3790a0b1423e6e3a31c789d8eb0fff4e27
Julian C. Dunn <jdunn@aquezada.com>
2014-03-23 21:09:46 -0400

    Update to Firefox 28.0

--- attributes/firefox.rb
+++ attributes/firefox.rb
@@ -1,2 +1,2 @@
-default['macapps']['firefox']['dmg']['source'] = 'http://download.mozilla.org/?product=firefox-27.0.1&os=osx&lang=en-US'
-default['macapps']['firefox']['dmg']['checksum'] = '49aab0add0a2323380539cb47bc2d901a48937d04d35882c05726c502eafda24'
+default['macapps']['firefox']['dmg']['source'] = 'http://download.mozilla.org/?product=firefox-28.0-SSL&os=osx&lang=en-US'
+default['macapps']['firefox']['dmg']['checksum'] = '287a90c5d067420fddbee365f4019c6ad5e931678955c5d36c6f4a0033616f9b'
