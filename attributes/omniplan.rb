bbf5a1b3f42ed1ce2e25773a895ec9a9d89c0571
Julian C. Dunn <jdunn@getchef.com>
2014-05-06 00:13:19 -0400

    Added Omniplan

---
+++ attributes/omniplan.rb
@@ -0,0 +1,2 @@
+default['macapps']['omniplan']['dmg']['source'] = 'http://downloads2.omnigroup.com/software/MacOSX/10.8/OmniPlan-2.3.5.dmg'
+default['macapps']['omniplan']['dmg']['checksum'] = '32f782a3d1e2b453b3789e870c92636bb5c3d8bc076d0d3232b03aaa4b339c8e'
