b6be06aea4dfd2e8f1458fdf7c99facff0d2ff89
Julian C. Dunn <jdunn@getchef.com>
2014-05-09 01:17:41 -0400

    Add azurecli recipe

---
+++ attributes/azurecli.rb
@@ -0,0 +1,2 @@
+default['macapps']['azurecli']['dmg']['source'] = 'http://az412849.vo.msecnd.net/downloads02/windowsazure-cli.0.8.3.dmg'
+default['macapps']['azurecli']['dmg']['checksum'] = 'b91254453d26c78d1483b07a7fcefc1c6f40c54b488413934fca9e5d450fcae8'
