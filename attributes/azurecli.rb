c2bebac8929cb9e764588118414b28ba981bba43
Julian C. Dunn <jdunn@aquezada.com>
2014-06-14 21:57:33 -0400

    Update to 0.8.4 of CLI

--- attributes/azurecli.rb
+++ attributes/azurecli.rb
@@ -1,2 +1,2 @@
-default['macapps']['azurecli']['dmg']['source'] = 'http://az412849.vo.msecnd.net/downloads02/windowsazure-cli.0.8.3.dmg'
-default['macapps']['azurecli']['dmg']['checksum'] = 'b91254453d26c78d1483b07a7fcefc1c6f40c54b488413934fca9e5d450fcae8'
+default['macapps']['azurecli']['dmg']['source'] = 'http://az412849.vo.msecnd.net/downloads02/windowsazure-cli.0.8.4.dmg'
+default['macapps']['azurecli']['dmg']['checksum'] = '6eb58dbc59ef7f69fd0182fea257f69c34d0e673c273592b439f451c5bd9a12b'
