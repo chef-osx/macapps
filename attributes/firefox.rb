5643999c13deb533509139a6a6c17c13d6cd707e
Julian C. Dunn <jdunn@aquezada.com>
2014-07-27 02:12:28 -0400

    Upgraded to Firefox 31.0

--- attributes/firefox.rb
+++ attributes/firefox.rb
@@ -1,2 +1,2 @@
-default['macapps']['firefox']['dmg']['source'] = 'https://download-installer.cdn.mozilla.net/pub/firefox/releases/29.0.1/mac/en-US/Firefox%2029.0.1.dmg'
-default['macapps']['firefox']['dmg']['checksum'] = '851701557eeacd6b1e5a087388f5b5462855c3e7944693e31690a922e23bda81'
+default['macapps']['firefox']['dmg']['source'] = 'https://download-installer.cdn.mozilla.net/pub/firefox/releases/31.0/mac/en-US/Firefox%2031.0.dmg'
+default['macapps']['firefox']['dmg']['checksum'] = '0110cad551fe5569df184a4b1a39b20819e1774d2802ab68deafcbce52e7d3fb'
