8d5f829686ec27de085d5bb116929b3e5e85bf58
Julian C. Dunn <jdunn@aquezada.com>
2014-08-29 21:34:45 -0400

    Update to calibre 2.1.0

--- attributes/calibre.rb
+++ attributes/calibre.rb
@@ -1,2 +1,2 @@
-default['macapps']['calibre']['dmg']['source'] = 'http://files.calibre-ebook.com/1.39.0/calibre-1.39.0.dmg'
-default['macapps']['calibre']['dmg']['checksum'] = 'ff9415a379fa8b9b2303b3ab82ece2b9e1f43d94a8a1523d68b857b2f2c7fe9e'
+default['macapps']['calibre']['dmg']['source'] = 'http://download.calibre-ebook.com/2.1.0/calibre-2.1.0.dmg'
+default['macapps']['calibre']['dmg']['checksum'] = '831fc759558cf3981d9e8817055bdbfc45bc44e7a4ad89b17d9a6809a1bc0f0d'
