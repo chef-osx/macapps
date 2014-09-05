4ba4b7bc924f56ad9603198b209c5e8a8278ac9c
Julian C. Dunn <jdunn@getchef.com>
2014-09-04 23:23:50 -0400

    Update to 1.6.5

--- attributes/vagrant.rb
+++ attributes/vagrant.rb
@@ -1,2 +1,2 @@
-default['macapps']['vagrant']['dmg']['source'] = 'https://dl.bintray.com/mitchellh/vagrant/vagrant_1.5.4.dmg'
-default['macapps']['vagrant']['dmg']['checksum'] = 'c8ded45183a03a8bfabe67ee82886ecfdafeb24030df9e631717ae1209475ba8'
+default['macapps']['vagrant']['dmg']['source'] = 'https://dl.bintray.com/mitchellh/vagrant/vagrant_1.6.5.dmg'
+default['macapps']['vagrant']['dmg']['checksum'] = 'a94a16b9ed5f63460f64110738067aea029238f8d826c8dd90c5c34615a5be1e'
