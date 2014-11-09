44ac65928b253d125eecfe9cc0cadb6880bba8c0
Julian C. Dunn <jdunn@getchef.com>
2014-11-09 16:48:06 -0500

    Update to Firefox 33.0.3

--- attributes/firefox.rb
+++ attributes/firefox.rb
@@ -1,2 +1,2 @@
-default['macapps']['firefox']['dmg']['source'] = 'https://download-installer.cdn.mozilla.net/pub/firefox/releases/31.0/mac/en-US/Firefox%2031.0.dmg'
-default['macapps']['firefox']['dmg']['checksum'] = '0110cad551fe5569df184a4b1a39b20819e1774d2802ab68deafcbce52e7d3fb'
+default['macapps']['firefox']['dmg']['source'] = 'https://download-installer.cdn.mozilla.net/pub/firefox/releases/33.0.3/mac/en-US/Firefox%2033.0.3.dmg'
+default['macapps']['firefox']['dmg']['checksum'] = 'dfb728ffa564c3f8558944e6ccf2e1e313d36afdb0d379d6900845743b9b2ad1'
