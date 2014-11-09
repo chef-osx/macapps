455071b1c99d4bcd97d78a7cc766dbdf9c4276cd
Julian C. Dunn <jdunn@getchef.com>
2014-11-08 22:10:52 -0500

    Added Omnigraffle

---
+++ attributes/omnigraffle.rb
@@ -0,0 +1,2 @@
+default['macapps']['omnigraffle']['dmg']['source'] = 'http://downloads2.omnigroup.com/software/MacOSX/10.8/OmniGraffle-6.0.5.dmg'
+default['macapps']['omnigraffle']['dmg']['checksum'] = 'a2eff19909d1ba38a4f01b2beecbde2f31f4af43d30e06d2c6921ae8880f85bc'
