334f511dbe45780e144d992fe34ad83a7961dfb7
Julian C. Dunn <jdunn@getchef.com>
2014-11-08 22:12:26 -0500

    Update to 2.3.7

--- attributes/omniplan.rb
+++ attributes/omniplan.rb
@@ -1,2 +1,2 @@
-default['macapps']['omniplan']['dmg']['source'] = 'http://downloads2.omnigroup.com/software/MacOSX/10.8/OmniPlan-2.3.5.dmg'
-default['macapps']['omniplan']['dmg']['checksum'] = '32f782a3d1e2b453b3789e870c92636bb5c3d8bc076d0d3232b03aaa4b339c8e'
+default['macapps']['omniplan']['dmg']['source'] = 'http://downloads2.omnigroup.com/software/MacOSX/10.8/OmniPlan-2.3.7.dmg'
+default['macapps']['omniplan']['dmg']['checksum'] = '54c4b12c480b2dc67aee942d44a3d7400e500b3bb656c7ce2c2eb89031896986'
