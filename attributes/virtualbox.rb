a80881ef9fb28695933348f516a4b5f594f108d5
Julian C. Dunn <jdunn@getchef.com>
2014-11-15 23:51:19 -0800

    Upgraded to 4.3.18

--- attributes/virtualbox.rb
+++ attributes/virtualbox.rb
@@ -1,2 +1,2 @@
-default['macapps']['virtualbox']['dmg']['source'] = 'http://download.virtualbox.org/virtualbox/4.3.12/VirtualBox-4.3.12-93733-OSX.dmg'
-default['macapps']['virtualbox']['dmg']['checksum'] = 'a9d9e3b3217177bc175839747de663ba25886f71e97a9c35ad802f618f11e23e'
+default['macapps']['virtualbox']['dmg']['source'] = 'http://download.virtualbox.org/virtualbox/4.3.18/VirtualBox-4.3.18-96516-OSX.dmg'
+default['macapps']['virtualbox']['dmg']['checksum'] = 'a0c6a9a9455397a929150065b2883714a1dd630f63ef88b8d8cf527944becfc6'
