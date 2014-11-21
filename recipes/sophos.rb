7e55a1dab0a0a558f751a45bc7b407c1269c12b6
Julian C. Dunn <jdunn@getchef.com>
2014-11-21 17:09:05 -0500

    Updated to Sophos 9.2

--- recipes/sophos.rb
+++ recipes/sophos.rb
@@ -16,13 +16,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-remote_file "#{Chef::Config[:file_cache_path]}/savosx_90_he.zip" do
-  source 'http://downloads.sophos.com/home-edition/savosx_90_he.zip'
-  checksum 'e5f1b8cd0afaaca689942fdfb693e53e4e0b0dc88442344a1b5e7b23ca823fd9'
+remote_file "#{Chef::Config[:file_cache_path]}/savosx_he_r.zip" do
+  source 'http://downloads.sophos.com/home-edition/savosx_he_r.zip'
+  checksum '86bcd0a10de1dc2455f3f1e9330c0b083e30ce435822daaa7ea10f04f63f5f10'
   notifies :run, "execute[unzip-sophos]"
 end
 
 execute "unzip-sophos" do
-  command "unzip #{Chef::Config[:file_cache_path]}/savosx_90_he.zip -d /Applications"
+  command "unzip #{Chef::Config[:file_cache_path]}/savosx_he_r.zip -d /Applications"
   action :nothing
 end
