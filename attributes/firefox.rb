9ebecbc4a6b2af150e1158144a46c7e9f5e7334d
Julian C. Dunn <jdunn@getchef.com>
2014-11-22 23:32:02 -0500

    Update to Firefox 31.1.1

--- attributes/firefox.rb
+++ attributes/firefox.rb
@@ -1,2 +1,2 @@
-default['macapps']['firefox']['dmg']['source'] = 'https://download-installer.cdn.mozilla.net/pub/firefox/releases/33.0.3/mac/en-US/Firefox%2033.0.3.dmg'
-default['macapps']['firefox']['dmg']['checksum'] = 'dfb728ffa564c3f8558944e6ccf2e1e313d36afdb0d379d6900845743b9b2ad1'
+default['macapps']['firefox']['dmg']['source'] = 'https://download-installer.cdn.mozilla.net/pub/firefox/releases/33.1.1/mac/en-US/Firefox%2033.1.1.dmg'
+default['macapps']['firefox']['dmg']['checksum'] = '5b3f4ff3de95399ba697dd42ac86a77f2eaeb00ee3461cd6bf500d170d949c5b'
