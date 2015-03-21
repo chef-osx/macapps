b4468f52d06b99d5f171f7a7f44bf431903b7357
Julian C. Dunn <jdunn@aquezada.com>
2015-03-20 20:43:01 -0400

    Update to latest gpgtools

--- attributes/gpgtools.rb
+++ attributes/gpgtools.rb
@@ -1,2 +1,2 @@
-default['macapps']['gpgtools']['dmg']['source'] = 'https://releases.gpgtools.org/GPG%20Suite%20-%202014.12-b4.dmg'
-default['macapps']['gpgtools']['dmg']['checksum'] = '711e175595fd4c1525de90b741fa0d02793df753465d27c6cefc35eb0573cd33'
+default['macapps']['gpgtools']['dmg']['source'] = 'https://releases.gpgtools.org/GPG_Suite-2015.03-b6.dmg'
+default['macapps']['gpgtools']['dmg']['checksum'] = '6621fc1da5211650b6ef4aa959fdd385a6a5a6d5'
