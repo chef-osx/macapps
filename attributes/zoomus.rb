c4a391105147d1cbeeb088af1b97a8c64fccabdc
Julian C. Dunn <jdunn@chef.io>
2015-03-23 12:04:57 -0400

    Update to new Zoom

--- attributes/zoomus.rb
+++ attributes/zoomus.rb
@@ -1,2 +1,2 @@
-default['macapps']['zoomus']['pkg']['source'] = 'http://d11yldzmag5yn.cloudfront.net/prod/3.5.6558.0105/zoomusInstaller.pkg'
-default['macapps']['zoomus']['pkg']['checksum'] = 'e9c89968ebfcee857ff3a35123848a77cd6318130a7a9f45f3fa155ce74d554e'
+default['macapps']['zoomus']['pkg']['source'] = 'http://d11yldzmag5yn.cloudfront.net/prod/3.5.11039.0317/zoomusInstaller.pkg'
+default['macapps']['zoomus']['pkg']['checksum'] = '2c4a26491433cf43292da865b09db07b4fba92a2a8a42e9f2547d7bc5f099330'
