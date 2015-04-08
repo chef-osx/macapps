8c7941f9b97463fce17e6fd9ab637228bcbb5b62
Julian C. Dunn <jdunn@chef.io>
2015-04-08 11:15:47 -0400

    Correct checksum

--- attributes/zoomus.rb
+++ attributes/zoomus.rb
@@ -1,2 +1,2 @@
 default['macapps']['zoomus']['pkg']['source'] = 'http://d11yldzmag5yn.cloudfront.net/prod/3.5.11039.0317/zoomusInstaller.pkg'
-default['macapps']['zoomus']['pkg']['checksum'] = '2c4a26491433cf43292da865b09db07b4fba92a2a8a42e9f2547d7bc5f099330'
+default['macapps']['zoomus']['pkg']['checksum'] = '9c76eb2da91e226189347d4cbfce3bc911b265a78b781a9744be2ddb30143ecf'
