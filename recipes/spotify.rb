20121c43287628200aa430673f7f7c460dbc7938
Julian C. Dunn <jdunn@chef.io>
2015-10-09 01:02:48 -0400

    Always overwrite

--- recipes/spotify.rb
+++ recipes/spotify.rb
@@ -23,6 +23,6 @@ remote_file "#{Chef::Config[:file_cache_path]}/SpotifyInstaller.zip" do
 end
 
 execute "unzip-spotify" do
-  command "unzip #{Chef::Config[:file_cache_path]}/SpotifyInstaller.zip -d /Applications"
+  command "unzip -o #{Chef::Config[:file_cache_path]}/SpotifyInstaller.zip -d /Applications"
   action :nothing
 end
