3ecc6e9b3d992821fba2240acd1819229d9b10a0
Julian C. Dunn <jdunn@aquezada.com>
2015-10-18 19:42:17 -0400

    Update to virtualbox 4.3.30.

--- attributes/virtualbox.rb
+++ attributes/virtualbox.rb
@@ -1,2 +1,2 @@
-default['macapps']['virtualbox']['dmg']['source'] = 'http://download.virtualbox.org/virtualbox/4.3.20/VirtualBox-4.3.20-96996-OSX.dmg'
-default['macapps']['virtualbox']['dmg']['checksum'] = '744e77119a640a5974160213c9912568a3d88dbd06a2fc6b6970070941732705'
+default['macapps']['virtualbox']['dmg']['source'] = 'http://download.virtualbox.org/virtualbox/4.3.30/VirtualBox-4.3.30-101610-OSX.dmg'
+default['macapps']['virtualbox']['dmg']['checksum'] = '1616b058f232683e74ecd85ed60aebf3c27901893b371596e66e9080a1493ccb'
